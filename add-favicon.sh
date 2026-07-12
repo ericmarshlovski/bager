538e127cd56167659f1c49a6c52a5ecc3051e0d0
rojaakademi <rojaakademi@proton.me>
2026-07-13 01:22:09 +0300

    x

--- add-favicon.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sed -i '' 's|<title>ba û bager | GNU/Linux Enthusiast</title>|<title>ba û bager | GNU/Linux Enthusiast</title>\n    <link rel="icon" type="image/png" href="favicon.png">|g' index.html
